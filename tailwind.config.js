3faa087c482894783079993ce2045a8cc89e0886
Naman Avasthi <naman.avasthi@gmail.com>
2021-10-02 21:38:58 -0700

    setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        inter: ['"Inter"', "sans-serif"],
+        deca: ['"Lexend Deca"', "sans-serif"],
+      },
+      colors: {
+        primary: {
+          blue: {
+            dark: "hsl(233, 47%, 7%)",
+            desaturated: "hsl(244, 38%, 16%)",
+          },
+          violet: "hsl(277, 64%, 61%)",
+        },
+        neutral: {
+          100: "hsla(0, 0%, 100%, 0.75)",
+          200: "hsla(0, 0%, 100%, 0.6)",
+        },
+      },
+    },
   },
   variants: {
     extend: {},
